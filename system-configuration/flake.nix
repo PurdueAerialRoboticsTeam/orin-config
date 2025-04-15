ffb30ea58dbdad2e16074c85c61b0a3751be4881
dardarman1 <64745732+dardarman1@users.noreply.github.com>
2025-04-15 19:53:21 -0400

    Update flake.nix

--- system-configuration/flake.nix
+++ system-configuration/flake.nix
@@ -2,18 +2,20 @@
   description = "Orin System Manager";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:PurdueAerialRoboticsTeam/feonix";
-
+    feonix.url = "github:PurdueAerialRoboticsTeam/feonix";
     system-manager = {
       url = "github:numtide/system-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, flake-utils, nixpkgs, system-manager }: {
+  outputs = { self, flake-utils, nixpkgs, feonix, system-manager }: {
     systemConfigs.default = system-manager.lib.makeSystemConfig {
       modules = [
         ./modules
+        # Import the Feonix NixOS module provided by your Feonix flake.
+        feonix.nixosModules.feonix
+        { services.feonix.enable = true; }
       ];
     };
   };
