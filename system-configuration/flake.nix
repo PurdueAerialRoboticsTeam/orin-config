4005d553bf82e85b5d0909a3004ad59086753642
dardarman1 <darealdardarman1@gmail.com>
2025-04-16 22:59:11 -0500

    enable ssh

--- system-configuration/flake.nix
+++ system-configuration/flake.nix
@@ -1,11 +1,17 @@
 {
   description = "Orin System Manager";
   inputs = {
+    feonix = {
+      type = "git";
+      url = "ssh://git@github.com/PurdueAerialRoboticsTeam/feonix.git";
+      ref = "master";
+      flake = true;
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    feonix.url = "github:PurdueAerialRoboticsTeam/feonix";
     system-manager = {
       url = "github:numtide/system-manager";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.url = "github:numtide/flake-utils"; 
     };
   };
 
